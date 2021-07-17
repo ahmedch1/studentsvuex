fa84dfba9b64bd8c1927bfdbca24eaab6a4ab396
Ahmed Ch <ahmedchouihi@gmail.com>
2021-07-17 13:45:41 +0100

    'makingAsynchronousCalls'

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
+import axios from "axios";
 
 Vue.use(Vuex);
 export default new Vuex.Store({
@@ -19,6 +20,12 @@ export default new Vuex.Store({
         },
         addStudent(state,student){
             state.students.push(student);
+        }
+    },
+    actions:{
+        async getStudents(context){
+            let students= (await axios.get('http://localhost:3000/students')).data;
+            context.commit('setStudents',students);
         }
     }
 })
